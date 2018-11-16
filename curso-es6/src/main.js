da4983c9385a1a613fe420d941e290354ebd2074
Heitor Neto <heitorh3@gmail.com>
2018-11-16 20:26:10 -0200

    Consultando o repo via rest

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,9 +1,12 @@
+import api from './api';
 
 class App {
     constructor(){
         this.repositories = [];
+        this.inputEl = document.querySelector('input[name=repositoryName]');
         this.formEl = document.getElementById('repo-form');
-        
+        this.listEl = document.getElementById('repo-list');
+
         this.registerHandlers();
     }
 
@@ -11,31 +14,56 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event){
+    async addRepository(event){
         event.preventDefault();
 
+        const repoName = this.inputEl.value;
+
+        if(repoName.length === 0 ){
+            return;
+        }
+
+        const response = await api.get(`search/repositories?q=${repoName}`);
+        //const response = await api.get(`user/repos/${repoName}`);
+        //const {name, description, html_url, owner: {avatar_url}} = response.data;
+        
+        console.log(response.data);
+        
         this.repositories.push({
-            name:'',
-            description:'',
-            avatar_url:'',
-            html_url:''
+            name,
+            description,
+            avatar_url,
+            html_url,
         });
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo =>{
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 new App();
-/*
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-
-            console.log(response);
-        }catch(err){
-            console.warn(err);
-        }
-    }
-}
-
-Api.getUserInfo('heitorh3');
-*/
