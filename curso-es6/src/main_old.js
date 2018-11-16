f0b8741178e0d6f945a2bf7ea4f47536a85b3514
Heitor Neto <heitorh3@gmail.com>
2018-11-16 10:10:59 -0200

    Instalando e configurando o axios

---
+++ curso-es6/src/main_old.js
@@ -0,0 +1,148 @@
+/*
+import { multiplicacao } from '../functions/funcoes';
+import soma from '../functions/soma';
+
+class TodoList {
+
+    constructor(){
+        this.todo = [];
+    }
+
+    addTodo(){
+        this.todo.push("Todo");
+    }
+
+    showTodo(){
+        console.log(this.todo);
+        return this.todo;
+    }
+}
+
+const todo = new TodoList();
+
+document.getElementById('addTodo').onclick = function() {
+    todo.addTodo();
+}
+
+document.getElementById('showTodo').onclick = function() {
+    var div = document.getElementById('display');
+    div.innerText = "<h1>" + todo.showTodo() +"</h1>"+ "\n";
+} 
+
+
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map(function(item, index){
+    return item * index;
+});
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
+
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map(item => item * 2);
+
+console.log(newArr);
+
+const teste = () => {
+    return 'Teste';
+}
+
+const soma = (a = 2, b = 5) => a + b;
+
+console.log(soma(1));
+console.log(soma());
+
+
+const usuario = {
+    nome: 'Heitor Neto',
+    idade: 34,
+    endereco: {
+        cidade: 'Uberlandia',
+        estado: 'Minas Gerais'
+    }
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function mostraNome({nome, idade}){
+    console.log(nome, idade);
+}
+
+console.log(usuario);
+
+const {nome, idade, endereco: {cidade}} = usuario;
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+mostraNome(usuario);
+
+
+//REST
+const {nome, ...resto} = usuario;
+console.log(nome);
+console.log(resto);
+
+const [a, b, c, ...d] = arr;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+function soma(...params){
+    return params.reduce((total, next) => total + next );
+}
+
+console.log(soma(1,2,3,4));
+
+//SPREAD
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+const usuario1 = {...usuario, nome: 'Joao de Deus'};
+console.log(usuario1);
+
+console.log(`2 x 2 = ${multiplicacao(2,2)}`);
+
+console.log(`3 + 3 = ${soma(3,3)}`);
+*/
+
+const minhaPromisse = () => new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve('ok')
+    }, 2000);
+});
+
+minhaPromisse().then(response => {
+    console.log(response);
+});
+
+async function executaPromisse(){
+    const response = await minhaPromisse();
+    alert(response);
+    //console.log(response);
+}
+
+executaPromisse();
