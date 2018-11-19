84de6ad6dcd7b3c366b2eef8aacd1b57d1bdc75c
Heitor Neto <heitorh3@gmail.com>
2018-11-19 21:30:24 -0200

    Configurando Nunjucks

--- gonode/index.js
+++ gonode/index.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const express = require('express');
-
+const nunjucks = require('nunjucks');
+/*
 http
     .createServer((req, res) =>{
         console.log(req);
         return res.end('Hello word!!');
     }).listen(3000);
-
+*/
 const app = express();
 
 const logMiddleware = (req, res, next) => {
@@ -17,10 +18,20 @@ const logMiddleware = (req, res, next) => {
     return next();
 }
 
-app.use(logMiddleware);
+//app.use(logMiddleware);
+
+nunjucks.configure('views',{
+    autoescape: true,
+    express: app,
+    watch:true,
+});
+
+app.set('view engine', 'njk');
 
 app.get('/',logMiddleware, (req, res) => {
-    return res.send(`Bem vindo ao ${req.appName}, ${req.query.name}`);
+    //return res.send(`Bem vindo ao ${req.appName}, ${req.query.name}`);
+    //Usando o nunjucks para renderizar o html
+    return res.render('list', { name: req.appName});
 });
 
 app.get('/nome/:name',(req, res) => {
