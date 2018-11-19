4dabe38e4ce3bcbd9871fb4a278eec8ae7460ddd
Heitor Neto <heitorh3@gmail.com>
2018-11-19 15:21:01 -0200

    Instalacao do ExpressJs

--- gonode/index.js
+++ gonode/index.js
@@ -1,6 +1,19 @@
 const http = require('http');
+const express = require('express');
 
-http.createServer((req, res) =>{
-    console.log(req);
-    return res.end('Hello word!!');
-}).listen(3000);
+http
+    .createServer((req, res) =>{
+        console.log(req);
+        return res.end('Hello word!!');
+    }).listen(3000);
+
+const app = express();
+
+app.get('/',(req, res) => {
+    return res.send(`Bem vindo, ${req.query.name}`);
+});
+
+app.get('/nome/:name',(req, res) => {
+    return res.send(`Bem vindo,  ${req.params.name}`);
+});
+app.listen(3001);
