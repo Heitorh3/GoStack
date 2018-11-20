00ae9cff8aed812970009238c70203c7fbc54fb9
Heitor Neto <heitorh3@gmail.com>
2018-11-20 18:12:17 -0200

    Configurando o eslint e prettier

--- desafio1/index.js
+++ desafio1/index.js
@@ -1,51 +1,50 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
 /* Configuracos do nunjucks */
-nunjucks.configure('views',{
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
-  watch:true,
-});
+  watch: true
+})
 
 const checkAgeQueryParam = (req, res, next) => {
   const { age } = req.query
 
   if (!age) {
-    return res.redirect('/');
+    return res.redirect('/')
   }
 
   return next()
 }
 
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'njk');
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
 app.get('/', (req, res) => {
-  return res.render('new');
-});
+  return res.render('new')
+})
 
-app.post('/check', checkAgeQueryParam,(req, res) => {
+app.post('/check', checkAgeQueryParam, (req, res) => {
   const { age } = req.body
 
-  if(age >= 18){
+  if (age >= 18) {
     return res.redirect(`/major?age=${age}`)
-  }else{
+  } else {
     return res.redirect(`/minor?age=${age}`)
   }
+})
 
-});
-
-app.get('/major',checkAgeQueryParam, (req, res) => {
+app.get('/major', checkAgeQueryParam, (req, res) => {
   const { idade } = req.query
-  return res.render('major', {idade});
-});
+  return res.render('major', { idade })
+})
 
-app.get('/minor',checkAgeQueryParam, (req, res) => {
+app.get('/minor', checkAgeQueryParam, (req, res) => {
   const { idade } = req.query
-  return res.render('minor', {idade});
-});
+  return res.render('minor', { idade })
+})
 
-app.listen(3000);
+app.listen(3000)
